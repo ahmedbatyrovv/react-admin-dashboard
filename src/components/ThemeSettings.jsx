a5ad05a4acfdc0cac6d79d6f77a0e7c3da6ce22e
Ahmed Batyrov <ahmed.batyrovv@gmail.com>
2026-04-15 17:29:11 +0300

    makimg settings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -7,11 +7,14 @@ import { themeColors } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
+  const { setColor, setMode, currentMode, currentColor, setThemeSettings } =
+    useStateContext();
+
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
       <div className="float-ring h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
         <div className="flex justify-between items-center p-4 ml-4">
-          <p className="font-semibold text-lg">Settings</p>
+          <p className="font-semibold text-xl">Settings</p>
           <button
             type="button"
             onClick={() => {}}
@@ -20,6 +23,66 @@ const ThemeSettings = () => {
           >
             <MdOutlineCancel />
           </button>
+        </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Options</p>
+
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              className="cursor-pointer"
+              onChange={() => {}}
+              checked={true}
+            />
+            <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+              Light
+            </label>
+          </div>
+
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="dark"
+              name="theme"
+              value="Dark"
+              className="cursor-pointer"
+              onChange={() => {}}
+              checked={true}
+            />
+            <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
+              Dark
+            </label>
+          </div>
+        </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Colors</p>
+          <div className="flex gap-3">
+            {themeColors.map((item, index) => (
+              <TooltipComponent
+                key={index}
+                content={item.name}
+                position="TopCenter"
+              >
+                <div className="relative mt-2 cursor-pointer flex gap-5 items-center">
+                  <button
+                    type="button"
+                    className="h-10 w-10 rounded-full cursor-pointer"
+                    style={{ backgroundColor: item.color }}
+                    onClick={() => {}}
+                  >
+                    <BsCheck
+                      className={`ml-2 text-2xl text-white ${true ? "block" : "hidden"}`}
+                    />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
         </div>
       </div>
     </div>
